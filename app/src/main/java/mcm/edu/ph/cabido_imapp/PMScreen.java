bdf9fd3b0364bd2e28f429f5bd7969af54958535
BeeCabs <beecabido@gmail.com>
2021-01-18 09:27:52 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/cabido_imapp/PMScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.cabido_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PMScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pmscreen);
+    }
+}
